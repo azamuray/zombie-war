72934d4110a6f1eaf9ef9f5e2dbd041cb43387f7
azamat <azamat.murdalov@mail.ru>
2018-09-07 04:08:47 +0300

    update zombie-war

--- zombie-war.py
+++ zombie-war.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 
 # Обновление:
-# Решил, что добавлять локации прямо в код не есть правильно,
-# поэтому решил создать отдельный текстовый файл, в котором
-# они будут храниться. Теперь можно просто прописать новую
-# локацию в текстовом файле 'locations.txt' и она появится в игре.
+# Наконец-то смог решить проблему добавления локаций.
+# Отныне не нужно в коде прописывать ни локацию, ни количество зомби.
+# Теперь можно добавить локацию и количество зомби в
+# файле 'locations.txt'.
+
 
 def change_location(location, count_zombie):
     print "Ты находишься в локации" , location
@@ -27,6 +28,7 @@ def change_location(location, count_zombie):
         print "--- В этой локации нет зомби. ---"
     return count_zombie
 
+
 # Создаю пустой массив для локаций
 locations = []
 
@@ -35,11 +37,25 @@ open_file = open('locations.txt', 'r')
 
 # Выгружаю все локации из файла 'locations.txt'
 # и помещаю эти локации в пустой массив 'locations[]'.
-for loc in open_file.readlines():
+for loc in open_file:
+    loc = loc[:-4]
+    loc = loc.strip('=')
     locations.append(loc.strip())
 
-# Количество зомби на каждую локацию.
-zombies = [2, 5, 8, 10]
+
+# Создаю пустой массив для количества зомби на каждую локацию
+zombies = []
+
+# Открываю текстовый файл 'locations.txt'.
+open_file = open('locations.txt', 'r')
+
+# Выгружаю все локации из файла 'locations.txt'
+# и помещаю эти локации в пустой массив 'zombies[]'.
+for zom in open_file:
+    zom = zom[-3:-1]
+    zom = zom.strip('=')
+    zombies.append(int(zom))
+
 
 print "Квестовая игра: Зомбиапокалипсис"
 print "Как играть: Вводишь цифру соответствующего пункта и нажимаешь ENTER."
@@ -48,28 +64,27 @@ print "  *** ИГРА НАЧАЛАСЬ ***"
 
 print "Город наполнен живыми мертвецами, тебе нужно передвигаться по локациям и убивать их."
 
+
 all_zombies = sum(zombies)
 
+# цикл, который не дает игре завершиться, пока не умрут все зомби
 while all_zombies > 0:
     print 20*"-"
-    print "Выберите локацию:"
+    print "Выбери локацию:"
     number_location = 1
+    # цикл, который выводит все локации
     for i in locations:
         print "  %d. %s" % (number_location, i)
         number_location += 1
     action = raw_input(" -> ")
-    if action == "1":
-        zombies[0] = change_location(locations[0], zombies[0])
-    elif action == "2":
-        zombies[1] = change_location(locations[1], zombies[1])
-    elif action == "3":
-        zombies[2] = change_location(locations[2], zombies[2])
-    elif action == "4":
-        zombies[3] = change_location(locations[3], zombies[3])
+    if action >= "1" and action <= str(len(locations)):
+        action = int(action) - 1
+        zombies[action] = change_location(locations[action], zombies[action])
     else:
         print "--- Неверная команда. ---"
     print "Всего зомби:", sum(zombies)
     all_zombies = sum(zombies)
+
 
 print ""
 print "Ты прошел все три локации."
